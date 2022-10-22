0880bdb892a7dd0b0442663934cb70cddd19d941
Elildes F Santos - Note <elildes.fortaleza.969@ufrn.edu.br>
2022-10-22 20:45:55 -0300

    create project spring

---
+++ backend/crudtarefas/src/main/java/com/imd/pds/crudtarefas/CrudtarefasApplication.java
@@ -0,0 +1,13 @@
+package com.imd.pds.crudtarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudtarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudtarefasApplication.class, args);
+	}
+
+}
